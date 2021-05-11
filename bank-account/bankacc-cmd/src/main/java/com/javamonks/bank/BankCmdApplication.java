5898bba459c3e3067424a131c1b1dd4fe809787d
shaelraj <shael.dhum88@gmail.com>
2021-05-11 22:43:08 +0530

    bank-account microservices

---
+++ bank-account/bankacc-cmd/src/main/java/com/javamonks/bank/BankCmdApplication.java
@@ -0,0 +1,13 @@
+package com.javamonks.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCmdApplication.class, args);
+	}
+
+}
---
+++ bank-account/bankacc-query/src/main/java/com/javamonks/bank/BankQueryApplication.java
@@ -0,0 +1,13 @@
+package com.javamonks.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankQueryApplication.class, args);
+	}
+
+}
