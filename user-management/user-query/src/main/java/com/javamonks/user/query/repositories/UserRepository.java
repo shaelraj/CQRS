5880026982a91f925bc943efe6c6cce5b2c7dba7
shaelraj <shael.dhum88@gmail.com>
2021-05-12 10:27:05 +0530

    corrected auth module

--- user-management/user-query/src/main/java/com/javamonks/user/query/repositories/UserRepository.java
+++ user-management/user-query/src/main/java/com/javamonks/user/query/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.javamonks.user.query.repositories;
 
 import com.javamonks.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
+    @Query("{'$or' : [{'firstname': {$regex : ?0, $options: '1'}}, {'lastname': {$regex : ?0, $options: '1'}}, {'emailAddress': {$regex : ?0, $options: '1'}}, {'account.username': {$regex : ?0, $options: '1'}}]}")
     Optional<User> findByFilterRegex(String filter);
 }
